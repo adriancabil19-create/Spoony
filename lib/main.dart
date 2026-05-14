346b633fe72f7883ef62e44c34e75889fd4f5a3f
Charles <you@example.com>
2026-05-15 06:44:48 +0800

    new maisna

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,9 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: const String.fromEnvironment('SUPABASE_URL', defaultValue: 'https://lapxlngpjrrmqscbvqjx.supabase.co'),
-    anonKey: const String.fromEnvironment('SUPABASE_ANON_KEY', defaultValue: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxhcHhsbmdwanJybXFzY2J2cWp4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3OTE2MDUsImV4cCI6MjA5NDM2NzYwNX0.Qx8FOwb2jlG0KGXwI58zSM8Oo3fP5ggsHnlHXTzpFyk'),
-  );
+  url: 'https://lapxlngpjrrmqscbvqjx.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxhcHhsbmdwanJybXFzY2J2cWp4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3OTE2MDUsImV4cCI6MjA5NDM2NzYwNX0.Qx8FOwb2jlG0KGXwI58zSM8Oo3fP5ggsHnlHXTzpFyk',
+);
 
   runApp(const ProviderScope(child: SpoonyApp()));
 }
